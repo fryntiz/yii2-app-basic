58172c23a5665298db5126445c7e42019d0dc4dc
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-05-19 19:15:02 +0200

    Planteado instalar requisitos

--- docs/Planteamiento_Inicial/install-required.sh
+++ docs/Planteamiento_Inicial/install-required.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 # -*- ENCODING: UTF-8 -*-
 
-instalar_ruby() {
-    LISTA=$(gem list --local)
-
-    for p in asciidoctor asciidoctor-pdf
-    do
-        if ! echo $LISTA | grep -qs "$p "
-        then
-            echo "Instalando $p..."
-            sudo gem install $p --pre
-        fi
-    done
+instalar_vendor() {
+    if [[ ! -d vendor ]]; then
+        composer install
+    else
+        composer update
+    fi
 }
 
 instalar_python() {
@@ -30,8 +25,22 @@ instalar_ghi() {
     sudo apt install -f ghi
 }
 
-instalar_ruby
+instalar_ruby() {
+    LISTA=$(gem list --local)
+
+    for p in asciidoctor asciidoctor-pdf
+    do
+        if ! echo $LISTA | grep -qs "$p "
+        then
+            echo "Instalando $p..."
+            sudo gem install $p --pre
+        fi
+    done
+}
+
+instalar_vendor
 instalar_python
 instalar_php
 instalar_composer
 instalar_ghi
+instalar_ruby
